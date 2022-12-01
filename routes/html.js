2aae5951a4758737e6771c4c0612d2ed1fb46df3
edyta0106 <edyta0106@gmail.com>
2022-12-01 09:05:18 -0700

    created README file

--- routes/html.js
+++ routes/html.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
 const path = require("path");
 
+// GET Route for notes
 router.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
+// GET Route for homepage
 router.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/index.html"));
 });
--- server.js
+++ server.js
@@ -2,15 +2,18 @@ const express = require("express");
 const api = require("./routes/api");
 const html = require("./routes/html");
 
+// Added additional PORT
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+// Any routes defined in a variable will prefix with / for html and /api for api
 app.use("/", html);
 app.use("/api", api);
 
